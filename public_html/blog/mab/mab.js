48f0738de6193f41b1b67d41d8f0e07575f38439
Dimitri Tishchenko <koozdra@gmail.com>
2024-09-26 21:48:11 -0700

    more details. simulation averages chart

--- public_html/blog/mab/mab.js
+++ public_html/blog/mab/mab.js
@@ -14,14 +14,25 @@ const colors = [
 const variantProbabilities = [0.1, 0.3, 0.7, 0.9];
 const horizon = 1000;
 const startingDollars = 1000;
+const selectionStrategies = ["random", "e_greedy", "ucb"];
 
 const getContext = (elementId) =>
   document.getElementById(elementId).getContext("2d");
 
-const setupCharts = (
+const runSimulation = async (config) =>
+  new Promise((resolve) => {
+    const worker = new Worker("worker.js");
+    worker.postMessage(config);
+
+    worker.addEventListener("message", function (e) {
+      worker.terminate();
+      resolve(e.data);
+    });
+  });
+
+const setupCharts = async (
   selectionStrategy,
   useOverTimeCanvasId,
-
   pieCanvasId,
   totalElementId
 ) => {
@@ -81,51 +92,89 @@ const setupCharts = (
     },
   });
 
-  const worker = new Worker("worker.js");
-
-  worker.postMessage({
-    variantProbabilities,
-    horizon,
-    startingDollars,
-    selectionStrategy,
-  });
-
-  worker.addEventListener("message", function (e) {
-    const { windowDistributions, occurrence, currentDollars } = e.data;
-    windowDistributions.forEach((series, index) => {
-      timeChart.config.data.datasets[index].data = series;
+  const { windowDistributions, occurrence, currentDollars } =
+    await runSimulation({
+      variantProbabilities,
+      horizon,
+      startingDollars,
+      selectionStrategy,
     });
-    console.log(occurrence);
-    pieChart.config.data.datasets[0].data = occurrence;
 
-    timeChart.update();
-    pieChart.update();
-
-    // const bestVariant
-
-    document.getElementById(totalElementId).innerHTML = currentDollars;
+  windowDistributions.forEach((series, index) => {
+    timeChart.config.data.datasets[index].data = series;
   });
+  pieChart.config.data.datasets[0].data = occurrence;
+
+  timeChart.update();
+  pieChart.update();
+
+  document.getElementById(totalElementId).innerHTML = currentDollars;
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  setupCharts(
-    "random",
-    "decision_distribution_over_time_random",
-    "decision_distribution_random",
-    "total_random"
-  );
+  selectionStrategies.map((strat) => {
+    setupCharts(
+      strat,
+      `decision_distribution_over_time_${strat}`,
+      `decision_distribution_${strat}`,
+      `total_${strat}`
+    );
+  });
 
-  setupCharts(
-    "e_greedy",
-    "decision_distribution_over_time_e_greedy",
-    "decision_distribution_e_greedy",
-    "total_e_greedy"
-  );
-
-  setupCharts(
-    "ucb",
-    "decision_distribution_over_time_ucb",
-    "decision_distribution_ucb",
-    "total_ucb"
-  );
+  clickGenerateSimulationAverages();
 });
+
+const clickGenerateSimulationAverages = async (button) => {
+  if (button) button.blur();
+  const numTrials = 100;
+  const simulationAverageChart = new Chart(getContext("simulation_averages"), {
+    type: "line",
+    data: {
+      labels: Array.from({ length: numTrials }, (_, i) => i + 1),
+      datasets: ["Random", "E-Greedy", "UCB"].map((label, index) => ({
+        label,
+        data: [],
+        borderColor: `rgba(${colors[index % colors.length]}, 1)`,
+      })),
+    },
+    options: {
+      animation: false,
+      plugins: {
+        tooltip: {
+          mode: "index",
+          intersect: false,
+        },
+      },
+    },
+  });
+
+  const totals = [];
+
+  const generateDataPoint = async (counter, max) => {
+    const results = await Promise.all(
+      selectionStrategies.map((selectionStrategy) =>
+        runSimulation({
+          variantProbabilities,
+          horizon,
+          startingDollars,
+          selectionStrategy,
+        })
+      )
+    );
+
+    results.map(({ currentDollars }, index) => {
+      totals[index] = (totals[index] || 0) + currentDollars;
+      simulationAverageChart.config.data.datasets[index].data.push(
+        totals[index] / (counter + 1)
+      );
+    });
+
+    simulationAverageChart.update();
+
+    if (counter < max) {
+      setTimeout(generateDataPoint, 0, counter + 1, max);
+    }
+  };
+
+  generateDataPoint(0, numTrials);
+};
